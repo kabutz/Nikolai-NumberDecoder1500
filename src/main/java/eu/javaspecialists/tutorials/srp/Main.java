172e5a627fcf527259f34c63a70f562d001903c2
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2022-10-13 11:50:41 +0300

    documented tasks to do

--- src/main/java/eu/javaspecialists/tutorials/srp/Main.java
+++ src/main/java/eu/javaspecialists/tutorials/srp/Main.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 import static java.lang.Character.*;
 
+// TODO: Read input from user
+// TODO: Determine if we are at the end
+// TODO: Validating the input (is it 10 characters)
+// TODO: Convert each character 0-9 -> 0-9, A-C -> 2, D-F -> 3, etc.
+// TODO: Format the final String
+// TODO: Send the final String somewhere, for example System.out
+// TODO: Test cases for the conversion and validation
+// TODO: Performance benchmark for conversion options
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static String number;
